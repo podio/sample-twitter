0249078877609f995599a79c72aa815185cd2aed
holm <holm@podio.com>
2012-05-03 12:54:46 +0200

    Use target blank

--- src/main/java/com/podio/sample/twitter/HTMLTweetPrinter.java
+++ src/main/java/com/podio/sample/twitter/HTMLTweetPrinter.java
@@ -4,6 +4,6 @@ public class HTMLTweetPrinter implements TweetPrinter {
 
 	@Override
 	public String getLink(String text, String url) {
-		return "<a href=\"" + url + "\">" + text + "</a>";
+		return "<a href=\"" + url + "\" target=\"_blank\">" + text + "</a>";
 	}
 }
